166b550168a3e0a02acd80dc9b86e32b52734095
luwuxy <marjosephbijer123@gmail.com>
2025-07-11 08:40:30 +0800

    testing image transitions

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ window.addEventListener("DOMContentLoaded", () => {
         outline.id = "active-box";
     }, 100)
 })
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const img = document.getElementById("art1");
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+img.onclick = function () {
+    modal.style.display = "flex";
+    modal.classList.toggle("dark");
+    modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+    modal.classList.toggle("dark");
+}
